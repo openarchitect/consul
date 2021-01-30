7fa1d692ebe1c26636a3e2b1f602b0d74fe70fea
openarchitect <9965150+openarchitect@users.noreply.github.com>
2021-01-30 18:11:33 +0100

    Fix merged content errors.

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,14 +21,8 @@ set :log_level, :info
 set :pty, true
 set :use_sudo, false
 
-<<<<<<< HEAD
-set :linked_files, %w{config/database.yml config/secrets.yml config/unicorn.rb config/environments/production.rb config/initializers/delayed_job_config.rb}
-set :linked_dirs, %w{log tmp public/system public/assets public/ckeditor_assets}
-
-=======
 set :linked_files, %w[config/database.yml config/secrets.yml]
 set :linked_dirs, %w[.bundle log tmp public/system public/assets public/ckeditor_assets]
->>>>>>> upstream/master
 
 set :keep_releases, 5
 
